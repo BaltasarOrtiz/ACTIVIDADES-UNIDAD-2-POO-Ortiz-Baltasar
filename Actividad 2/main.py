df8df418a38ef5e5f8b31318ced6baeb84a1e10c
BaltasarOrtiz <103899892+BaltasarOrtiz@users.noreply.github.com>
2022-04-24 15:29:54 -0300

    Add files via upload

---
+++ Actividad 2/main.py
@@ -0,0 +1,15 @@
+import csv
+from ClaseViajeros import Viajero
+from menu import llamarMenu
+
+if __name__== '__main__':
+    listaViajero = []
+    archivo = open("ArchViajeros.csv")
+    reader = csv.reader(archivo, delimiter=";")
+    next(reader)
+    for fila in reader:
+        v = Viajero(fila[0], fila[1], fila[2], fila[3], fila[4])
+        listaViajero.append(v)
+    
+    #print(len(listaViajero))
+    llamarMenu(listaViajero)
