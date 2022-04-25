60719306eb9b7ab527f706cb8a95f4e6fee41311
BaltasarOrtiz <103899892+BaltasarOrtiz@users.noreply.github.com>
2022-04-25 12:16:28 -0300

    Add files via upload

--- Actividad 3/ClaseRegistro.py
+++ Actividad 3/ClaseRegistro.py
@@ -1,13 +1,22 @@
 
 class registro:
-    __varTemp: float
-    __varHum: float
-    __varPres: float
+    __varTemp = float
+    __varHum = float
+    __varPres = float
     
     
     def __init__(self, temp, hum, pres):
         self.__varTemp = temp
         self.__varHum = hum
-        self.__varPress = pres
-        
+        self.__varPres = pres
+    
+    
+    def getTemp(self):
+        return self.__varTemp
 
+    def getHum(self):
+        return self.__varHum
+    
+    def getPres(self):
+        return self.__varPres
+    
--- Actividad 3/main.py
+++ Actividad 3/main.py
@@ -1,6 +1,28 @@
 
-
-import ClaseRegistro
+import csv
+from ClaseRegistro import registro
+from menu import llamarMenu
 
 if __name__== '__main__': 
+    
+    dia = 30
+    hora = 24
+    lista = []
+    
+    for i in range(dia):
+        lista.append([0]*hora)
+    
+    archivo = open("temperaturas.csv")
+    reader = csv.reader(archivo, delimiter=",")
+    next(reader)
+    for line in reader:
+        dia = int(line[0])
+        hora = int(line[1])
+        temp = line[2]
+        hum = line[3]
+        pres = line[4]
+        lista[dia-1][hora-1] = registro(float(temp), float(hum), float(pres))
+    
+    
+    llamarMenu(lista)
     
